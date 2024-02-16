aa0a2046da676c82414715ecbd6b1391bb99db8f
bsramosl <steeven10f@gmail.com>
2024-02-16 16:04:43 -0500

    cambio plantilla

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
+import { ResumenComponent } from './pages/resumen/resumen.component';
 import { WorksComponent } from './pages/works/works.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 const routes: Routes = [
   {path: '', component:HomeComponent}, 
   {path:'about',component:AboutComponent},
-  {path:'works',component:WorksComponent}
+  {path:'resumen',component:ResumenComponent},
+  {path:'works',component:WorksComponent},
+  {path:'contact',component:ContactComponent},
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
+import { ResumenComponent } from './pages/resumen/resumen.component';
 import { WorksComponent } from './pages/works/works.component';
-
+import { ContactComponent } from './pages/contact/contact.component';
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     AboutComponent,
-    WorksComponent
+    ResumenComponent,
+    WorksComponent,
+    ContactComponent,
+    
   ],
   imports: [
     BrowserModule,
